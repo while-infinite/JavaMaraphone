83b7be5a3704d73d9cd64e94ef5e083b2871204a
Карен Фарманян < >
2022-04-15 17:00:02 +0300

    complete day 15 tasks

---
+++ src/main/java/day14/Person.java
@@ -0,0 +1,28 @@
+package day14;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import org.apache.commons.lang3.math.NumberUtils;
+
+    public class Person {
+        private String name;
+        private int year;
+
+        public Person(String name, int year) {
+            this.year = year;
+            this.name = name;
+        }
+
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "name='" + name + '\'' +
+                    ", year=" + year +
+                    '}';
+        }
+    }
+
